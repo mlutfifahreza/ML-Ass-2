7d42a3c2427e36f2f73ee8f5d6e05882a699e8da
Muhammad Fahreza <mlutfifahreza@gmail.com>
2021-05-25 13:55:54 +0700

    training done

--- main.py
+++ main.py
@@ -1,153 +1,68 @@
 import cv2 as cv
 import random
 from matplotlib import pyplot as plt
-from mlp import *
 import os
 
-datasets_path = 'downloads/flowers-datasets/'
+datasets_path = '/content/drive/MyDrive/Semester 6/Machine Learning/Tugas/Assignment 2 - MLP/flowers-datasets/'
 
 # (1) ARSITEKTUR MLP 
 # total input = 320*240 = 76800
 width = 320
 height = 240
 dim = (width,height)
-# total neuron di hidden (1 layer)
-n_hidden = 240
+n_hidden = 40 # total neuron di hidden (1 layer)
 
 # (3) LOAD DATASETS
+print("import database")
 targets = os.listdir(datasets_path)
 if '.DS_Store' in targets: #remove clutter from mac folder file
     targets.remove('.DS_Store')
 print('targets :',targets)
 
-images = {}
-images_flat = {}
+encoding = {
+    "dandelion" : [1,0,0],
+    "rose" : [0,1,0],
+    "sunflower" : [0,0,1]
+}
+
+print("getting images")
+images = []
 for target in targets:
-    images[target] = []
-    images_flat[target] = [[],[]] # pixel value, target [0,1,0]
     image_names = os.listdir(datasets_path+target)
     if '.DS_Store' in image_names: #remove clutter from mac folder file
         image_names.remove('.DS_Store')
     for item in image_names:
-        # get the image
-        img = cv.imread(datasets_path+target+'/'+item)        
+        img = cv.imread(datasets_path+target+'/'+item)  # get the image
 # (4) GRAYSCALE
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # resize image
         img = cv.resize(img, dim)
-        img1d = img.flatten()
-        target1d = [0, 0, 0]
-        # add each image to corresponding target in images
-        images[target].append(img)
-        # add to image_flat
-        if target == "dandelion":
-            target1d = [1, 0, 0]
-        elif target == "rose":
-            target1d = [0, 1, 0]
-        else:
-            target1d = [0, 0, 1]
+        # add each image,encoding to images
+        images.append([img,encoding[target]])
 
-        images_flat[target].append([img1d, target1d])
-
-## SPLIT TRAINING TESTING DATA
-images_training_data = {}
-images_testing_data = {}
-images_training_data_1d = {}
-images_testing_data_1d = {}
-
-images_dummy = images
-images_flat_dummy = images_flat
-
-for target in targets:
-    random.shuffle(images_dummy[target])
-    random.shuffle(images_flat_dummy[target])
-
-for target in targets:
-    images_training_data.append(images_dummy[target][:80])
-    images_training_data_1d.append(images_flat_dummy[target][:80])
-    images_testing_data.append(images_dummy[target][80:])
-    images_testing_data_1d.append(images_flat_dummy[target][80:])
-
-print(len(images_training_data))
-print(len(images_testing_data))
-
-# (5) VISUALISASI DATA
-# print("Sample image (2 each)")
-# for target in targets:
-#     print(target)
-#     for i in range(1):
-#         print("image",i)
-#         # cv.imshow('image',images[target][i])
-#         plt.imshow(images[target][i],cmap="gray")
-#         plt.show()
-#     print()
-#     # input("enter to continue")
-# print(images)
-# image = {
-#     "dandelion" : []
-#     "rose"
-#     "sunflower"
-# }
-# image["rose"][4]
-
-fig = plt.figure(figsize=(10, 7))
-  
-# setting values to rows and column variables
-rows = 1
-columns = 3
-
-fig.add_subplot(rows, columns, 1)
-# showing image
-plt.imshow(images["dandelion"][0])
-plt.axis('off')
-plt.title("Dandelion")
-  
-# Adds a subplot at the 2nd position
-fig.add_subplot(rows, columns, 2)
-  
-# showing image
-plt.imshow(images["rose"][0])
-plt.axis('off')
-plt.title("Rose")
-  
-# Adds a subplot at the 3rd position
-fig.add_subplot(rows, columns, 3)
-  
-# showing image
-plt.imshow(images["sunflower"][0])
-plt.axis('off')
-plt.title("Sunflower")
-
-
-# #important library to show the image 
-# # import matplotlib.image as mpimg
-# # import matplotlib.pyplot as plt
-# #importing numpy to work with large set of data.
-# import numpy as np
-# #image read function
-# img=cv.imread('Downloads/flowers-datasets/rose353897245_5453f35a8e.jpg')
-# #image sclicing into 2D. 
-# x=img[:,0]
-# # x co-ordinate denotation. 
-# plt.xlabel("Value")
-# # y co-ordinate denotation.
-# plt.ylabel("pixels Frequency")
-# # title of an image .
-# plt.title("Original Image")
-# # imshow function with comperision of gray level value.
-# plt.imshow(x,cmap="gray")
-# #plot the image on a plane.
-# plt.show()
-# plt.title("HIstogramm for given Image'  ")
-# plt.xlabel("Value")
-# plt.ylabel("pixels Frequency")
-# #hist function is used to plot the histogram of an image.
-# plt.hist(x)
-
-
-# (2) DEFINISIKAN ARSITEKTUR
-# flower_mlp = mlp(dim, n_hidden, targets)
 
 # (12) SPLIT TRAIN AND TEST DATA
+import random
+print("split data")
+def split_data(data):
+    n_shuffle = 4
+    n_data = len(data)
+    for i in range(n_shuffle):
+        random.shuffle(data)
+    boundary = int(0.8 * n_data)
+    return data[0:boundary], data[boundary:n_data]
 
-# (13) VISUALISASI : Error dan akurasi per epoch dg a=0.1 dan a=0.8
+train_data, test_data = split_data(images)
+print("total train data =",len(train_data))
+print("total test data =",len(test_data))
+
+# (2) DEFINISIKAN ARSITEKTUR
+print("new mlp")
+flower_mlp = mlp(dim, n_hidden, targets, 50, 0.1)
+print("new mlp training")
+flower_mlp.train(train_data)
+
+# # (5) VISUALISASI DATA
+
+
+# # (13) VISUALISASI : Error dan akurasi per epoch dg a=0.1 dan a=0.8
--- mlp.py
+++ mlp.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import time
 
 # Multi Layer Perceptron
 class mlp:
@@ -8,7 +10,7 @@ class mlp:
         z = np.dot(input, weight)
         return 1/(1 + np.exp(-z))
 
-    def __init__(self, dim, n_hidden, output, epoch, alpha):
+    def __init__(self, dim, n_hidden, targets, epoch, alpha):
         # (2) DEFINISIKAN ARSITEKTUR
         # input layer
         self.width = dim[0]
@@ -17,22 +19,21 @@ class mlp:
         self.input = np.zeros(self.n_input) # save input value
         
         # hidden layer
-        self.hidden = np.zeros(self.n_hidden) # save activation value for hidden
         self.n_hidden = n_hidden + 1 # +1 for bias
+        self.hidden = np.zeros(self.n_hidden) # save activation value for hidden
         
         # output layer
-        self.output = np.zeros(self.n_hidden) # save activation value for output
-        self.n_output = len(output)
-        
+        self.n_output = len(targets)
+        self.output = np.zeros(self.n_output) # save activation value for output
         
         # (6) BACK PROB : Init bobot dan bias
         # init weight [weight0, weight1]
         self.weight = [[],[]]
         # init weight 0 (input..hidden) = 0.5
-        self.weight[0] = np.full((self.n_input, self.n_hidden), 0.5)
+        self.weight[0] = np.full((self.n_hidden, self.n_input), 0.5)
         # init weight 1 (hidden..output) = 0.5
-        self.weight[1] = np.full((self.n_hidden, self.n_output), 0.5)
-        # init bias(last index) = 1 
+        self.weight[1] = np.full((self.n_output, self.n_hidden), 0.5)
+        # set bias(last index) = 1 
         self.input[self.n_input-1] = 1
         self.hidden[self.n_hidden-1] = 1
 
@@ -42,15 +43,16 @@ class mlp:
         # training log
         self.epoch = epoch
         self.alpha = alpha
-        train_log = {'error' : [], 'accuracy' : []}
+        self.train_log = {'error' : [], 'accuracy' : []}
 
     # (7) BACK PROB : Hitung error
-    def mse(self, predictions, targets):
+    def mse(self, activations, targets):
         mse = 0
-        n = len(predictions)
+        n = len(activations)
         for i in range(n):
-            mse += (predictions[i] - targets[i])**2
-        mse /= n
+            for j in range(self.n_output):
+                mse += (activations[i][j] - targets[i][j])**2
+        mse /= n*self.n_output
         return mse
         
     # (8) BACK PROB : Feed-forward
@@ -69,16 +71,16 @@ class mlp:
         for j in range(self.n_output):
             for i in range(self.n_hidden):
                 a = self.hidden[i]
-                derivative_ij = 2*(a-target[j])*a*(1-a) * self.weight[1][i][j]
+                derivative_ij = 2*(a-target[j])*a*(1-a) * self.weight[1][j][i]
                 # update weight1 ij
-                self.weight[1][i][j] -= derivative_ij
+                self.weight[1][j][i] -= self.alpha * derivative_ij
                 # update derivative of neuron i on hidden layer
                 self.derivative[i] += derivative_ij
         # update weight input..hidden
         for j in range(self.n_hidden):
             for i in range(self.n_input):
                 # update weight0 ij
-                self.weight[0][i][j] -= self.derivative[j]
+                self.weight[0][j][i] -= self.alpha * self.derivative[j]
 
     # (10) BACK PROB : Prediksi
     def predict(self, input):
@@ -96,4 +98,29 @@ class mlp:
             return [0,0,1]
           
     def train(self, datasets):
-        for i in range(epoch):
+        for e in range(self.epoch):
+            start_time = time.perf_counter()
+            print("training #"+str(e))
+            activations = []
+            targets = []
+            n = len(datasets)
+            for i in range(n):
+                new_input = datasets[i][0].flatten()
+                new_input = np.append(new_input, 1) # adding bias
+                self.input = new_input
+                self.feed_forward()
+                self.feed_backward(datasets[i][1])
+                activations.append(self.output)
+                targets.append(datasets[i][1])
+                # check performance
+                done = i+1
+                undone = n-done
+                end_time = time.perf_counter()
+                elapsed_time = end_time - start_time
+                remaining_time = undone/done*elapsed_time
+                elapsed_time = "{:.2f}".format(elapsed_time)
+                remaining_time = "{:.2f}".format(remaining_time)
+                sys.stdout.write('\r'+"Progress : "+ str(done) + "/"+ str(n) + " - Elapsed = "+ str(elapsed_time) + "s - Remaining = " + str(remaining_time) + "s")
+            mse = self.mse(activations, targets)
+            print("Error =",mse)
+            self.train_log['error'].append(mse)
